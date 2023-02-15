9c49ff8f154709e03632875ee7ce6adf13c8bfe5
Sumaiah Binta Musa <sumaiah1905@cseku.ac.bd>
2023-02-15 08:47:20 +0600

    Source Code

---
+++ Bangla_Spelling_Checker/src/main/java/com/mycompany/bangla_spelling_checker/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bangla_spelling_checker;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
