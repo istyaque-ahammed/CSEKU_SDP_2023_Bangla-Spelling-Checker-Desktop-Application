ab5f4bbcffad649e2d24769c3e744219c2b36190
Istyaque Ahammed <istyaque2040@cseku.ac.bd>
2023-03-15 01:06:45 +0600

    New project

---
+++ Bangla-Spelling-Checker/src/Main.java
@@ -0,0 +1,6 @@
+package Bangla_Spelling_Checker;
+public class Main {
+    public static void main(String[] args) {
+        new introduction();
+    }
+}
---
+++ Bangla-Spelling-Checker/src/introduction.java
@@ -0,0 +1,74 @@
+package Bangla_Spelling_Checker;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+public class introduction extends JFrame {
+    JFrame frame,demo;
+    JLabel image = new JLabel(new ImageIcon("src\\Images\\Logo.png"));
+    JLabel text = new JLabel("শুদ্ধ বাংলা বানান চর্চা");
+    JProgressBar progressBar = new JProgressBar();
+    JLabel message = new JLabel();
+    introduction() {
+        //create GUI
+        frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.setUndecorated(true);
+        frame.setBounds(350, 25, 650, 680);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setVisible(true);
+        image.setBounds(55, 25, 550, 400);
+        frame.add(image);
+        frame.getRootPane().setBorder(
+                BorderFactory.createMatteBorder(4, 2, 1, 3, Color.white)
+        );
+
+        progressBar.setBounds(125, 450, 400, 28);
+        progressBar.setBorderPainted(true);
+        progressBar.setStringPainted(true);
+        progressBar.setBackground(Color.DARK_GRAY);
+        progressBar.setForeground(Color.gray);
+        progressBar.setValue(0);
+        frame.add(progressBar);
+        message.setBounds(235, 475, 200, 40);
+        message.setForeground(Color.white);
+        message.setFont(new Font("Kalpurush", Font.BOLD, 20));
+        frame.add(message);
+        text.setFont(new Font("Kalpurush", Font.PLAIN, 40));
+        text.setBounds(180, 510, 600, 75);
+        text.setForeground(Color.white);
+        frame.add(text);
+        running();
+
+    }
+    //Loading Bar
+    public void running() {
+        int i = 0;
+        while (i <= 100) {
+            try {
+                Thread.sleep(100);
+                progressBar.setValue(i);
+                message.setText("দয়া করে অপেক্ষা করুন");
+                i++;
+                if (i >= 50) {
+                    i = i + 2;
+                }
+                else if(i >= 75) {
+                    i = i + 4;
+                }
+                if (i == 100) {
+                    frame.dispose();
+                    //  Notepad home_page = new Notepad();
+                  //  home_page.setVisible(true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
